836bc084374b00524d599efcab3d7d5eecd8690e
titch <sujinlee0202@gmail.com>
2024-12-23 02:54:25 +0900

    feat: post 불러오기 및 좋아요 기능 api 추가

--- src/post.ts
+++ src/post.ts
@@ -27,6 +27,10 @@ postsRouter.get("/", async (req: any, res: any) => {
             username: true,
           },
         },
+        likes: true,
+        _count: {
+          select: { likes: true },
+        },
       },
     });
     if (!post) {
@@ -37,6 +41,68 @@ postsRouter.get("/", async (req: any, res: any) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "서버 오류" });
+  }
+});
+
+// post id 로 post 불러오기
+postsRouter.get("/:id", async (req: any, res: any) => {
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: Number(req.params.id) },
+      include: {
+        author: {
+          select: {
+            username: true,
+          },
+        },
+        likes: true,
+        _count: {
+          select: { likes: true },
+        },
+      },
+    });
+
+    if (!post) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류" });
+  }
+});
+
+postsRouter.post("/likes", async (req, res: any) => {
+  const { userId, postId } = req.body;
+  console.log(userId, postId);
+  try {
+    const like = await prisma.like.create({
+      data: {
+        userId: userId,
+        postId: postId,
+      },
+    });
+
+    res.status(200).json({ like });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to like post" });
+  }
+});
+
+postsRouter.delete("/likes", async (req, res: any) => {
+  const { userId, postId } = req.body;
+  try {
+    const like = await prisma.like.deleteMany({
+      where: {
+        userId: userId,
+        postId: postId,
+      },
+    });
+
+    res.status(200).json({ like });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to unlike post" });
   }
 });
 
